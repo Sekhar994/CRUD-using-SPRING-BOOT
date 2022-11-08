9372029703ed19adfe535426d47503cb7ebbdedb
Rajasekhar Reddy Yeruva <52159632+Sekhar994@users.noreply.github.com>
2022-11-08 10:25:03 +0530

    Add files via upload

---
+++ src/main/java/com/crud/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.crud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seqno;
+    private String name;
+    private String department;
+    private String phoneno;
+    private String designation;
+    private String salary;
+}
